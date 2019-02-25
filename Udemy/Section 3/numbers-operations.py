81f9000b940c8b1d6b828c2bc25f3450113bb720
ctramm <ctramm@wiley.com>
2019-02-25 08:58:26 -0600

    Add more Section 3 files Work on numbers-operations

---
+++ Udemy/Section 3/numbers-operations.py
@@ -0,0 +1,20 @@
+# This is a one line comment
+
+# Exponentiation
+# 10 * 10 ... 20
+exponents = 10 ** 20
+print(exponents)
+
+expo = 5 ** 25
+print(expo)
+
+"""
+This is a multi-line comment
+Modulo - It returns the remainder
+"""
+
+remainder = 11 % 3
+print(remainder)
+
+rem = 15 % 2
+print(rem)
