80b64387317ca780987fef91582947dce2b78c0d
ctramm <ctramm@wiley.com>
2019-02-26 13:17:56 -0600

    Change listdemo

--- Udemy/Section 4/listdemo.py
+++ Udemy/Section 4/listdemo.py
@@ -3,7 +3,7 @@ Data type to store more than one value in one variable name
 List items are in brackets, separated with "," [ 1, 2, 3 ]
 """
 
-cars = [ "bmw", "honda", "audi"]
+cars = ["bmw", "honda", "audi"]
 empty_list = []
 print(empty_list)
 print(cars)
@@ -17,7 +17,7 @@ sum_num = num_list[0] + num_list[1]
 
 print(sum_num)
 
-more_cars = [ "bmw", "honda", "audi"]
+more_cars = ["bmw", "honda", "audi"]
 print(more_cars[1])
 
 more_cars[1] = "Benz"
