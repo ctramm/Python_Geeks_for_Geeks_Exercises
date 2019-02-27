a4353c8c7d80c4745ea6fd3ae5a518bcfec5efd4
ctramm <ctramm@wiley.com>
2019-02-27 12:29:03 -0600

    End Section 4

--- Udemy/Section 4/tuplesdemo.py
+++ Udemy/Section 4/tuplesdemo.py
@@ -5,18 +5,25 @@ It means you can't change them
 """
 
 my_list = [1, 2, 3]
+print("Print my list:")
 print(my_list)
 
 my_list[0] = 0
+print("\nPrint my list after changing original list:")
 print(my_list)
 
 my_tuple = (1, 2, 3, 2, 2, 3)
+print("\nPrint my tuple:")
 print(my_tuple)
 
+print("\nAccessing my tuple 0th index: ")
 print(my_tuple[0])
 
+print("\nSlicing my tuple starting at 1st index: ")
 print(my_tuple[1:])
 
+print("\nPrint index of value 2 within my tuple: ")
 print(my_tuple.index(2))
 
+print("\nPrint count of my tuple: ")
 print(my_tuple.count(3))
