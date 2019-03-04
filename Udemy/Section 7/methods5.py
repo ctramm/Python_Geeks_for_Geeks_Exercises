84b4616423cb93a4e7397f884b8ee6cac4d97edf
ctramm <ctramm@wiley.com>
2019-03-04 10:26:35 -0600

    Section 7: Practice Exercise - Calculate Net Income

--- Udemy/Section 7/methods5.py
+++ Udemy/Section 7/methods5.py
@@ -23,11 +23,11 @@ def state_tax(state, gross):
 
     states = {'AL': 0.05, 'AK': 0, 'AZ': 0.0454, 'AR': 0.069, 'CA': 0.15, 'CO': 0.0463,
               'CT': 0.0699, 'DE': 0.056, 'FL': 0, 'GA': 0, 'HI': 0, 'ID': 0, 'IL': 0,
-              'IN': 0, 'IA': 0, ' KS': 0, 'KY': 0, 'LA': 0, 'ME': 0, 'MD': 0, 'MA': 0,
-              'MI': 0, 'MN': 0, ' MS': 0, 'MO': 0, 'MT': 0, 'NE': 0, 'NV': 0, 'NH': 0,
-              'NJ': 0, 'NM': 0, ' NY': 0, 'NC': 0, 'ND': 0, 'OH': 0, 'OK': 0, 'OR': 0,
-              'PA': 0, 'RI': 0, ' SC': 0, 'SD': 0, 'TN': 0, 'TX': 0, 'UT': 0, 'VT': 0,
-              'VA': 0, 'WA': 0, ' WV': 0, 'WI': 0, 'WY': 0}
+              'IN': 0, 'IA': 0, 'KS': 0, 'KY': 0, 'LA': 0, 'ME': 0, 'MD': 0, 'MA': 0,
+              'MI': 0, 'MN': 0, 'MS': 0, 'MO': 0, 'MT': 0, 'NE': 0, 'NV': 0, 'NH': 0,
+              'NJ': 0, 'NM': 0, 'NY': .09, 'NC': 0, 'ND': 0, 'OH': 0, 'OK': 0, 'OR': 0,
+              'PA': 0, 'RI': 0, 'SC': 0, 'SD': 0, 'TN': 0, 'TX': 0, 'UT': 0, 'VT': 0,
+              'VA': 0, 'WA': 0, 'WV': 0, 'WI': 0, 'WY': 0}
 
     # Fed Tax
     net = gross - (gross * .10)
@@ -44,39 +44,59 @@ def state_tax(state, gross):
 
 
 # Test Code
-in_state = 'AL'
-income = 5000
-state_tax(in_state, income)
+# in_state = 'AL'
+# income = 5000
+# state_tax(in_state, income)
+#
+# in_state = 'AZ'
+# income = 5000
+# state_tax(in_state, income)
+#
+# in_state = 'AR'
+# income = 5000
+# state_tax(in_state, income)
+#
+# in_state = 'CA'
+# income = 5000
+# state_tax(in_state, income)
+#
+# in_state = 'CO'
+# income = 5000
+# state_tax(in_state, income)
+#
+# in_state = 'CT'
+# income = 5000
+# state_tax(in_state, income)
+#
+# in_state = 'DE'
+# income = 5000
+# state_tax(in_state, income)
+#
+# in_state = 'TX'
+# income = 5000
+# state_tax(in_state, income)
+#
+# in_state = 'ZZ'
+# income = 5000
+# state_tax(in_state, income)
 
-in_state = 'AZ'
-income = 5000
-state_tax(in_state, income)
-
-in_state = 'AR'
-income = 5000
-state_tax(in_state, income)
-
+print("California")
 in_state = 'CA'
-income = 5000
+income = 100000
 state_tax(in_state, income)
 
-in_state = 'CO'
-income = 5000
+print("\nNew York")
+in_state = 'NY'
+income = 100000
 state_tax(in_state, income)
 
-in_state = 'CT'
-income = 5000
+print("\nNew Jersey")
+in_state = 'NJ'
+income = 100000
 state_tax(in_state, income)
 
-in_state = 'DE'
-income = 5000
-state_tax(in_state, income)
-
+print("\nTexas")
 in_state = 'TX'
-income = 5000
-state_tax(in_state, income)
-
-in_state = 'ZZ'
-income = 5000
+income = 100000
 state_tax(in_state, income)
 
