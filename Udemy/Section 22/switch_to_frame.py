6a57273ffb08c9af2b6350d95480e9e954e46a1b
ctramm <ctramm@wiley.com>
2019-03-28 11:10:37 -0500

    Start Section 22 - iFrame Example is broken for practice website

--- Udemy/Section 22/switch_to_frame.py
+++ Udemy/Section 22/switch_to_frame.py
@@ -13,17 +13,27 @@ class SwitchToFrame:
         driver.maximize_window()
         driver.get(base_url)
         driver.implicitly_wait(3)
+        driver.execute_script("window.scrollBy(0, 1000);")
 
         # Switch to frame using ID
+        driver.switch_to.frame("courses-iframe")
 
         # Switch to frame using name
 
         # Switch to frame using numbers
 
+        time.sleep(5)
         # Search Course
-
         search = driver.find_element_by_id("search-courses")
         search.send_keys("python")
+        time.sleep(5)
+
+        # Switch back to the parent handle/frame
+        driver.switch_to.default_content()
+        driver.execute_script("window.scrollBy(0, -1000);")
+        time.sleep(5)
+        driver.find_element_by_id("name").send_keys("Test Successful")
+        time.sleep(5)
 
 
 c = SwitchToFrame()
